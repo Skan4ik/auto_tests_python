cf0358ee59394372885f7c4ea4631d2ec9108d80
Skan4ik <skan4ik1337@yandex.ru>
2023-02-03 12:23:24 +0500

    stepik task 2_2 step 2

---
+++ Lesson_2_2_step_2.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import math
+from selenium.webdriver.common.by import By
+import time
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/execute_script.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "body > div.container:nth-child(2) > form > div.form-group:nth-child(1) > label:nth-child(1) > span#input_value.nowrap:nth-child(2)")
+    x = x_element.text
+    y = calc(x)
+    # Код JS для скролла страницы вниз для видимости кнопки
+    button = browser.find_element(By.TAG_NAME, "button")
+    browser.execute_script("return arguments[0].scrollIntoView(true);", button)
+    button.click()
+
+    input1 = browser.find_element(By.CSS_SELECTOR, "div.container .form-group input")
+    input1.send_keys(y)
+    option1 = browser.find_element(By.CSS_SELECTOR, "[for='robotCheckbox']")
+    option1.click()
+    option2 = browser.find_element(By.CSS_SELECTOR, "[for='robotsRule']")
+    option2.click()
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
